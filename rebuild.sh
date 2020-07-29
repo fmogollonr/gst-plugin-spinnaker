79ba7fe14a8c8cc1ca4d04955fb3620dafacc03d
fmogollon <fmogollon@vicomtech.org>
2020-07-29 13:39:07 +0200

    Multiarch

--- rebuild.sh
+++ rebuild.sh
@@ -2,6 +2,7 @@
 sudo make uninstall
 make clean
 make distclean
-./configure --prefix=/usr/lib/x86_64-linux-gnu/gstreamer-1.0/ --libdir=/usr/lib/x86_64-linux-gnu/
+architecture=`dpkg --print-architecture`
+./configure --prefix=/usr/lib/$architecture-linux-gnu/gstreamer-1.0/ --libdir=/usr/lib/$architecture-linux-gnu/
 make -j4 
 sudo make install
